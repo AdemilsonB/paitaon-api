2f9bf02160bb2b81378863d98c3c39c4f48a3567
Ademilson Bertolin <ademilsonbertolinnn@gmail.com>
2023-06-25 15:37:43 -0300

    init api paitaon

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware("auth:api");
+    }
+
+    public function me() {
+        return response()->json(Auth::guard('api')->user()); //verifica autenticação e obtém uma resposta Json com dados do user;
+    }
+
+}
